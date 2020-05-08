9b247357978aef718ba5ceb622d051aab315869c
langheran <langheran@gmail.com>
2020-05-07 22:36:52 -0500

    changes

--- kernel_list.bat
+++ kernel_list.bat
@@ -3,6 +3,7 @@ pip install octave_kernel
 jt -t oceans16 -f fira -fs 9 -nf ptsans -nfs 7 -N -kl -cursw 3 -cursc r -cellw 95% -T
 pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension 
+pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install
 
 ipython kernelspec list
 pause
