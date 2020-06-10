854b1d356c3cee6dbda441c71ad24ba2e37339b0
langheran <langheran@gmail.com>
2020-06-09 22:14:26 -0500

    changes

--- kernel_list.bat
+++ kernel_list.bat
@@ -1,9 +1,15 @@
-pip install jupyterlab
-pip install octave_kernel
-jt -t oceans16 -f fira -fs 9 -nf ptsans -nfs 7 -N -kl -cursw 3 -cursc r -cellw 95% -T
-pip install ipywidgets
-jupyter nbextension enable --py widgetsnbextension 
-pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install
+REM pip install jupyterlab
+REM pip install octave_kernel
+REM pip install ipywidgets
+REM dotnet tool install -g dotnet-try
+REM dotnet try jupyter install
+REM dotnet tool install  --version 2.1.3
+REM jupyter nbextension enable --py widgetsnbextension 
+REM pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install
+REM jt -t oceans16 -f fira -fs 9 -nf ptsans -nfs 7 -N -kl -cursw 3 -cursc r -cellw 95% -T
 
 ipython kernelspec list
 pause
+
+REM choco install icsharp
+REM jupyter kernelspec uninstall icsharpkernel
